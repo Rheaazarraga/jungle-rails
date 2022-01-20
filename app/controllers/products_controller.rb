400a55806ad5186715634ca457f1bb506bc1974f
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-20 00:37:14 +0000

    Add http basic auth

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['JUNGLE_USERNAME'], password: ENV['JUNGLE_PASSWORD']
 
   def index
     @products = Product.all.order(created_at: :desc)
