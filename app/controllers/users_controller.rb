31b9939a9b5d7ef5735b7e7eefb23e680a1fcfdc
Rheaazarraga <rheaazarraga@gmail.com>
2022-01-23 01:09:58 +0000

    Add logic to create action and implement private method to sanitize the input from the form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,16 @@ class UsersControllerController < ApplicationController
   end
 
   def create
+    user = User.new(user_params)
+    if user.save
+      session[:user_id] = user.id
+      redirect_to '/signup'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
   
 end
